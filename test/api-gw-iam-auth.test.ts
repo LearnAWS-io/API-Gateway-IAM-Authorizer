c9de4a420deba94f37e064cd2b3a2b9b2574bf59
Shivam <singhshivamkr@gmail.com>
2024-03-11 11:25:50 +0530

    Initial commit

---
+++ test/api-gw-iam-auth.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApiGwIamAuth from '../lib/api-gw-iam-auth-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/api-gw-iam-auth-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApiGwIamAuth.ApiGwIamAuthStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
